7d95a17bd61988544498a6f0293beecd7e05677f
Mattemyo <matias97marti@gmail.com>
2018-01-29 22:02:40 +0100

    add images to slideshow, set up animation parameters

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,46 +1,66 @@
 import React from "react";
 import "./Slideshow.css";
+// Import Slides
+import buffe from "./images/buffe.jpg";
+import cerdoToledo from "./images/cerdoToledo.jpg";
+import GotebTapas from "./images/GotebTapas.jpg";
+import GotebTapasQueso from "./images/GotebTapasQueso.jpg";
+import GotebTapasQueso2 from "./images/GotebTapasQueso2.jpg";
+import GotebTapasQuesoMembrillo from "./images/GotebTapasQuesoMembrillo.jpg";
+import maricoPaella2 from "./images/maricoPaella2.jpg";
+import mariscoPaella from "./images/mariscoPaella.JPG";
+import norbertoGambas from "./images/norbertoGambas.jpg";
+import norbertoPaellas from "./images/norbertoPaellas.JPG";
+import paellaSmiley from "./images/paellaSmiley.jpg";
+import tapasJamon from "./images/tapasJamon.jpg";
 
-const Slideshow = () => (
-  <div>
-    <div id="landing-header">
-      <h1>Välkomen till Esmia</h1>
+const Slideshow = () => {
+  const images = [
+    buffe,
+    cerdoToledo,
+    GotebTapas,
+    GotebTapasQueso,
+    GotebTapasQueso2,
+    GotebTapasQuesoMembrillo,
+    maricoPaella2,
+    mariscoPaella,
+    norbertoGambas,
+    norbertoPaellas,
+    paellaSmiley,
+    tapasJamon
+  ];
+
+  const listedImages = images.map((image, i) => {
+    const animationLength = 8;
+    const liStyle = {
+      animation: `imageAnimation ${animationLength *
+        images.length}s linear infinite`,
+      animationDelay: `${i * animationLength}s`
+    };
+
+    return (
+      <li style={liStyle} key={image}>
+        <img src={image} />
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <div id="landing-header">
+        <h1>Välkomen till Esmia</h1>
+      </div>
+      <ul className="slideshow">{listedImages}</ul>
+      <div className="w-20">
+        <a
+          className="w-20 grow f4 link ph3 pv2 dib white bg-light-purple"
+          href="#0"
+        >
+          Tryck Här
+        </a>
+      </div>
     </div>
-    <ul className="slideshow">
-      <li>
-        <img
-          src="https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
-          alt="food"
-        />
-      </li>
-      <li>
-        <img
-          src="https://images.pexels.com/photos/660282/pexels-photo-660282.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
-          alt="food"
-        />
-      </li>
-      <li>
-        <img
-          src="https://images.pexels.com/photos/89778/strawberries-frisch-ripe-sweet-89778.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
-          alt="food"
-        />
-      </li>
-      <li>
-        <img
-          src="https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
-          alt="food"
-        />
-      </li>
-      <li>
-        <img
-          src="https://images.pexels.com/photos/8500/food-dinner-pasta-spaghetti-8500.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"
-          alt="food"
-        />
-      </li>
-    </ul>
-
-    <button>Tryck Här!</button>
-  </div>
-);
+  );
+};
 
 export default Slideshow;
