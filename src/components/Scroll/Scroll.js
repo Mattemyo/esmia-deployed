2cdab5e8b38bf0b0665bebed2bb93deea30a4f21
Mattemyo <matias97marti@gmail.com>
2018-02-08 21:43:40 +0100

    change color of scrollbar

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -4,7 +4,7 @@ export default class Scroll extends Component {
   render() {
     return (
       <div
-        className="mb4 list-container w-100 w-80-ns center"
+        className="mb4 list-container w-90 w-80-ns center"
         style={{ overflowY: "scroll", height: "800px", borderTop: '1px solid rgba(0, 0, 0, 0.2)' }}
       >
         {this.props.children}
