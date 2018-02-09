89496a8f70f2e1c0d9ac71f364a1b77601dccc70
Mattemyo <matias97marti@gmail.com>
2018-02-09 23:31:27 +0100

    update info page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,16 +3,16 @@ import "./Contact.css";
 import InfoCard from "../InfoCard/InfoCard";
 
 const Contact = ({ onRouteChange }) => (
-  <div className="w-90 w-30-l center">
-  <InfoCard>
-    <div className="tc">
-      <h1>Kontakt</h1>
-      <p>Email: norberto@esmia.se</p>
-      <p>Telefon: 070-7223191</p>
-      <p>Gustafsvägen 1, Solna</p>
-      <p>Öppettider: 8.00-18.00</p>
-    </div>
-  </InfoCard>
+  <div className="w-90 w-50-m w-30-l center">
+    <InfoCard>
+      <div className="tc">
+        <h1>Kontakt</h1>
+        <p>norberto@esmia.se</p>
+        <p>070-7223191</p>
+        <p>Gustafsvägen 1, Solna</p>
+        <p>8.00-18.00</p>
+      </div>
+    </InfoCard>
   </div>
 );
 
